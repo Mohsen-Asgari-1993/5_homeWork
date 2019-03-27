8f5a88d3375168a149bddfca1fcef821558ac8f0
mohsen <mohsen1993asgari@gmail.com>
2019-03-27 08:27:18 +0430

    Override display information

--- src/H_W_5/Tamrin1/Radio.java
+++ src/H_W_5/Tamrin1/Radio.java
@@ -5,4 +5,9 @@ public class Radio extends ElectronicsEquipments {
     public Radio(String brand, double price, String energyGrade){
         super(brand,price,energyGrade);
     }
+
+    @Override
+    public String displayInformation(){
+        return ("Brand: " + super.brand +"\t\tEnergy Grade: "+super.energyGrade+"\t\tPrice: "+super.price);
+    }
 }
