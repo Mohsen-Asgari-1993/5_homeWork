fff5b398709a42ae5f903be3fdbbabb39152b5c4
mohsen <mohsen1993asgari@gmail.com>
2019-03-27 08:31:08 +0430

    Adds properties and constructor

--- src/H_W_5/Tamrin1/Shoe.java
+++ src/H_W_5/Tamrin1/Shoe.java
@@ -1,4 +1,14 @@
 package H_W_5.Tamrin1;
 
-public class Shoe {
+public class Shoe extends Products {
+    private int size;
+    private String color;
+    private String type;
+
+    public Shoe(String brand, double price, String type, int size, String color) {
+        super(brand, price);
+        this.type=type;
+        this.size = size;
+        this.color = color;
+    }
 }
