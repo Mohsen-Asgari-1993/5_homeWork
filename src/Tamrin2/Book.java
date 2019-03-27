5181ffe8649338a96a5fcbbc23c4ec0e72b63138
mohsen <mohsen1993asgari@gmail.com>
2019-03-27 17:53:13 +0430

    Adds getter, abstract setPrice and display Information

--- src/Tamrin2/Book.java
+++ src/Tamrin2/Book.java
@@ -8,4 +8,18 @@ public abstract class Book {
     public Book(String title) {
         this.title = title;
     }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public abstract void setPrice();
+
+    public void displayInformation() {
+        System.out.println("Book's Title : " + this.title + "\t\tPrice : " + this.price);
+    }
 }
