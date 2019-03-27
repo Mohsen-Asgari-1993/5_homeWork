733d5499128bd92e89c2b984fd33b0d149b6f805
mohsen <mohsen1993asgari@gmail.com>
2019-03-27 08:15:58 +0430

    Adds properties and Constructor

--- src/H_W_5/Tamrin1/TV.java
+++ src/H_W_5/Tamrin1/TV.java
@@ -1,4 +1,12 @@
 package H_W_5.Tamrin1;
 
 public class TV extends ElectronicsEquipments {
+    private int inch;
+    private String type;
+
+    public TV(String brand, double price,String energyGrade, int inch , String type) {
+        super(brand, price, energyGrade);
+        this.inch = inch;
+        this.type=type;
+    }
 }
