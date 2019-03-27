09c94ed22ac8534f3a7a9eae8333509e8cb15666
mohsen <mohsen1993asgari@gmail.com>
2019-03-27 08:37:46 +0430

    Adds properties and constructor

--- src/H_W_5/Tamrin1/Readable.java
+++ src/H_W_5/Tamrin1/Readable.java
@@ -1,4 +1,11 @@
 package H_W_5.Tamrin1;
 
 public class Readable extends Products {
+    private Person person;
+    private String subject;
+
+    public Readable(String brand, double price, Person person) {
+        super(brand, price);
+        this.person = person;
+    }
 }
