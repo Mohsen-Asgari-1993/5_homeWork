a0ddd78d0c220a266f98d97c953265d304b1bd8c
mohsen <mohsen1993asgari@gmail.com>
2019-03-27 13:33:34 +0430

    Get First and Last Name from user call setInformation

--- src/H_W_5/Tamrin1/Main.java
+++ src/H_W_5/Tamrin1/Main.java
@@ -1,7 +1,20 @@
 package H_W_5.Tamrin1;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("please enter your first name:");
+        String firstName = scanner.next();
+
+        System.out.println("please enter your last name:");
+        String lastName = scanner.next();
+
+        Person person = new Person(firstName, lastName);
+        person.setInformation();
+
         
     }
 }
