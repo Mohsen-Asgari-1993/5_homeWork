14c9f80dc7261cd5ac4608347962352c27d9cc08
mohsen <mohsen1993asgari@gmail.com>
2019-03-27 18:04:21 +0430

    Creates instances of Fiction and NonFiction Call Display Information

--- src/Tamrin2/UseBook.java
+++ src/Tamrin2/UseBook.java
@@ -2,6 +2,9 @@ package Tamrin2;
 
 public class UseBook {
     public static void main(String[] args) {
-        
+        Fiction fiction = new Fiction("Troy");
+        NonFiction nonFiction = new NonFiction("ZIZI");
+        fiction.displayInformation();
+        nonFiction.displayInformation();
     }
 }
