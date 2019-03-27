6ec7b5a13dea056c1653ddc637ed181795718c40
mohsen <mohsen1993asgari@gmail.com>
2019-03-27 19:51:16 +0430

    Book Array implementation

--- src/Tamrin2/BookArray.java
+++ src/Tamrin2/BookArray.java
@@ -2,6 +2,15 @@ package Tamrin2;
 
 public class BookArray {
     public static void main(String[] args) {
-        
+        Book[] books = new Book[10];
+        char ch='A';
+        for (int i=0, j=1;i<=8;i+=2,j+=2){
+            books[i]=new Fiction(""+ch);
+            ch++;
+            books[j]=new NonFiction(""+ch);
+            ch++;
+        }
+        for (Book b:books)
+            b.displayInformation();
     }
 }
