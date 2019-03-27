f0489882a71530c7219eb6bc00a83025a74e87cf
mohsen <mohsen1993asgari@gmail.com>
2019-03-27 08:26:44 +0430

    Adds Constructor

--- src/H_W_5/Tamrin1/Radio.java
+++ src/H_W_5/Tamrin1/Radio.java
@@ -1,4 +1,8 @@
 package H_W_5.Tamrin1;
 
 public class Radio extends ElectronicsEquipments {
+
+    public Radio(String brand, double price, String energyGrade){
+        super(brand,price,energyGrade);
+    }
 }
