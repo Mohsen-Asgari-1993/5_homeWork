22d9da19267405022f9488669a37ae5e9dcfba59
mohsen <mohsen1993asgari@gmail.com>
2019-03-27 17:51:41 +0430

    Adds properties and constructor

--- src/Tamrin2/Book.java
+++ src/Tamrin2/Book.java
@@ -1,4 +1,11 @@
 package Tamrin2;
 
 public abstract class Book {
+
+    protected String title;
+    protected double price;
+
+    public Book(String title) {
+        this.title = title;
+    }
 }
