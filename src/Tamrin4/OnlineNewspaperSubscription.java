0cc0f34ffc7ed907a5814bdd98e5c8521dc95d11
mohsen <mohsen1993asgari@gmail.com>
2019-03-28 11:46:10 +0430

    Adds Constructor

--- src/Tamrin4/OnlineNewspaperSubscription.java
+++ src/Tamrin4/OnlineNewspaperSubscription.java
@@ -1,4 +1,9 @@
 package Tamrin4;
 
 public class OnlineNewspaperSubscription extends NewspaperSubscription {
+
+    public OnlineNewspaperSubscription(String name, String address) {
+        super.name = name;
+        setAddress(address);
+    }
 }
