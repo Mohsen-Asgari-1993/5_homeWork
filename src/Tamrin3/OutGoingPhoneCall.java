8c3ea9ac94ab8eee354ee9a9fa90e7a58ab870aa
mohsen <mohsen1993asgari@gmail.com>
2019-03-28 10:54:14 +0430

    Override setPrice and other abstract method

--- src/Tamrin3/OutGoingPhoneCall.java
+++ src/Tamrin3/OutGoingPhoneCall.java
@@ -13,4 +13,26 @@ public class OutGoingPhoneCall extends PhoneCall {
     public int getTime(){
         return this.time;
     }
+
+    @Override
+    public void setPrice() {
+        super.price=0.04;
+    }
+
+    @Override
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    @Override
+    public double getPrice() {
+        return 0.04;
+    }
+
+    @Override
+    public void displayInformation() {
+        System.out.println("Phone Number : "+getPhoneNumber()+"\trate per minute :"+getPrice()+"\tNumber of minutes : "+this.time +"\tTotal price :"+this.time*getPrice());
+
+    }
+    
 }
