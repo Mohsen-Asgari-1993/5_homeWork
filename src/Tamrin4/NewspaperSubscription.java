eea4c338298598132db290ea5ebc23269e17d83b
mohsen <mohsen1993asgari@gmail.com>
2019-03-28 11:30:07 +0430

    Adds getter and setter

--- src/Tamrin4/NewspaperSubscription.java
+++ src/Tamrin4/NewspaperSubscription.java
@@ -4,4 +4,22 @@ public abstract class NewspaperSubscription {
     protected String name;
     protected String address;
     protected String rate;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public abstract void setAddress(String address);
+
+    public String getRate() {
+        return rate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
