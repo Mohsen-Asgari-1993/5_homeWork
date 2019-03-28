985309d80468a4e63ba811d39135bff830a92b46
mohsen <mohsen1993asgari@gmail.com>
2019-03-28 10:47:42 +0430

    Adds property and constructor

--- src/Tamrin3/OutGoingPhoneCall.java
+++ src/Tamrin3/OutGoingPhoneCall.java
@@ -1,4 +1,12 @@
 package Tamrin3;
 
 public class OutGoingPhoneCall extends PhoneCall {
+
+    private int time;
+
+    public OutGoingPhoneCall(String phoneNumber, int time) {
+        super(phoneNumber);
+        setPrice();
+        this.time = time;
+    }
 }
