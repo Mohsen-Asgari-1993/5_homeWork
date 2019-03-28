eb507831e25b0a8f9e541df2d5c8e712a86981a2
mohsen <mohsen1993asgari@gmail.com>
2019-03-28 12:18:51 +0430

    Creates Instances of Metal and Non metal

--- src/Tamrin5/MetalElements.java
+++ src/Tamrin5/MetalElements.java
@@ -2,7 +2,7 @@ package Tamrin5;
 
 public class MetalElements extends Elements {
 
-    public MetalElement(String symbol, int atomicNumber, double atomicWeight){
+    public MetalElements(String symbol, int atomicNumber, double atomicWeight){
         super(symbol,atomicNumber,atomicWeight);
     }
 
