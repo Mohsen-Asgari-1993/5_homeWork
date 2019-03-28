cb58551b280fe762925d9f02afd650b0093e675a
mohsen <mohsen1993asgari@gmail.com>
2019-03-28 12:06:31 +0430

    Adds constructor

--- src/Tamrin5/NonMetal.java
+++ src/Tamrin5/NonMetal.java
@@ -1,4 +1,8 @@
 package Tamrin5;
 
 public class NonMetal extends Elements {
+
+    public NonMetal(String symbol, int atomicNumber, double atomicWeight) {
+        super(symbol, atomicNumber, atomicWeight);
+    }
 }
