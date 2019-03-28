97b9ffb22d7a55de4ed6f153664eb2839d38c522
mohsen <mohsen1993asgari@gmail.com>
2019-03-28 12:02:46 +0430

    Adds Constructor

--- src/Tamrin5/MetalElements.java
+++ src/Tamrin5/MetalElements.java
@@ -1,4 +1,8 @@
 package Tamrin5;
 
 public class MetalElements extends Elements {
+
+    public MetalElement(String symbol, int atomicNumber, double atomicWeight){
+        super(symbol,atomicNumber,atomicWeight);
+    }
 }
