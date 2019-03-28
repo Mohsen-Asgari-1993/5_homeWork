885d979aadaecc0ea2c39b8fcd57d0c7793cbcad
mohsen <mohsen1993asgari@gmail.com>
2019-03-28 11:59:17 +0430

    Adds properties and constructor

--- src/Tamrin5/Elements.java
+++ src/Tamrin5/Elements.java
@@ -1,4 +1,14 @@
 package Tamrin5;
 
 public abstract class Elements {
+    protected String symbol;
+    protected int atomicNumber;
+    protected double atomicWeight;
+
+    public Elements(String symbol, int atomicNumber, double atomicWeight) {
+        this.symbol = symbol;
+        this.atomicNumber = atomicNumber;
+        this.atomicWeight = atomicWeight;
+
+    }
 }
