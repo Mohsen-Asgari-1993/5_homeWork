9e146ec46eb53b81cf3b39ddbe2c9deef203ae94
mohsen <mohsen1993asgari@gmail.com>
2019-03-28 10:27:51 +0430

    Adds properties and constructor

--- src/Tamrin3/PhoneCall.java
+++ src/Tamrin3/PhoneCall.java
@@ -1,4 +1,12 @@
 package Tamrin3;
 
 public abstract class PhoneCall {
+
+    protected String phoneNumber;
+    protected double price;
+
+    public PhoneCall(String phoneNumber){
+        this.phoneNumber=phoneNumber;
+        this.price=0.0;
+    }
 }
