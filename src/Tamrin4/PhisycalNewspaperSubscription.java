e206e58607fe3c4a33516c7e1df07619eb4db0d5
mohsen <mohsen1993asgari@gmail.com>
2019-03-28 11:43:34 +0430

    Override setAddress

--- src/Tamrin4/PhisycalNewspaperSubscription.java
+++ src/Tamrin4/PhisycalNewspaperSubscription.java
@@ -6,4 +6,19 @@ public class PhisycalNewspaperSubscription extends NewspaperSubscription {
         super.name = name;
         setAddress(address);
     }
+
+    @Override
+    public void setAddress(String address) {
+        super.address = address;
+        outer:
+        for (int i = 0; i <= 9; i++) {
+            if (address.contains(i + "")) {
+                super.rate = "$15";
+                break outer;
+            } else if (i == 9) {
+                super.rate = "$0";
+                System.out.println("Error : wrong address imported");
+            }
+        }
+    }
 }
