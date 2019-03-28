1e25e2db5578efb180e62a2aeb1447f6c2a29282
mohsen <mohsen1993asgari@gmail.com>
2019-03-28 11:42:32 +0430

    Adds Constructor

--- src/Tamrin4/PhisycalNewspaperSubscription.java
+++ src/Tamrin4/PhisycalNewspaperSubscription.java
@@ -1,4 +1,9 @@
 package Tamrin4;
 
 public class PhisycalNewspaperSubscription extends NewspaperSubscription {
+
+    public PhisycalNewspaperSubscription(String name, String address) {
+        super.name = name;
+        setAddress(address);
+    }
 }
