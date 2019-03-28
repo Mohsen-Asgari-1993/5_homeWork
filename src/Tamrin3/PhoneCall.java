5b526a1f87c1ca11a68978777637e1f753a45299
mohsen <mohsen1993asgari@gmail.com>
2019-03-28 10:29:57 +0430

    Adds setPrice adds three abstract method

--- src/Tamrin3/PhoneCall.java
+++ src/Tamrin3/PhoneCall.java
@@ -9,4 +9,14 @@ public abstract class PhoneCall {
         this.phoneNumber=phoneNumber;
         this.price=0.0;
     }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public abstract String getPhoneNumber();
+
+    public abstract double getPrice();
+
+    public abstract void displayInformation();
 }
